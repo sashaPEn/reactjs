26d865b5621efa7d74f920f53417c78f716c06ff
sashaPEn <88935397+sashaPEn@users.noreply.github.com>
2022-04-18 18:06:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
+  constructor(props) }
+  {
     super(props);
     this.state = { value: "coconut" };
 
